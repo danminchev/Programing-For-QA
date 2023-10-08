43d963d04837409135638a093954ea592f9c9be4
Daniel Minchev <lbdidko@gmail.com>
2023-10-08 16:46:24 +0300

    Decision

--- For-Loops/05.DivisionTo2,3and4/05.DivisionTo2,3and4.cs
+++ For-Loops/05.DivisionTo2,3and4/05.DivisionTo2,3and4.cs
@@ -1,2 +1,32 @@
-﻿int num = int.Parse(Console.ReadLine());
+﻿int numbersCount = int.Parse(Console.ReadLine());
+
+double numbersDivide2 = 0;
+double numbersDivide3 = 0;
+double numbersDivide4 = 0;
+
+for (int i = 0; i < numbersCount; i++)
+{
+    int numbers = int.Parse(Console.ReadLine());
+
+    if (numbers % 2 == 0)
+    {
+        numbersDivide2++;
+    }
+    if (numbers % 3 == 0)
+    {
+        numbersDivide3++;
+    }
+    if (numbers % 4 == 0)
+    {
+        numbersDivide4++;
+    }
+}
+double pursent2 = numbersDivide2 / numbersCount * 100;
+double pursent3 = numbersDivide3 / numbersCount * 100;
+double pursent4 = numbersDivide4 / numbersCount * 100;
+
+Console.WriteLine($"{pursent2:f2}%");
+Console.WriteLine($"{pursent3:f2}%");
+Console.WriteLine($"{pursent4:f2}%");
+
 
