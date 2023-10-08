b68b58a86961fe76a9b4c53bf45647c080c13ba3
Daniel Minchev <lbdidko@gmail.com>
2023-10-09 00:21:48 +0300

    balance

---
+++ For-Loops/08.AccountBalance/08.AccountBalance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double balance = 0;
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "End")
+            {
+                break;
+            }
+
+            double amount = double.Parse(input);
+            balance += amount;
+
+            if (amount > 0)
+            {
+                Console.WriteLine($"Increase: {amount:f2}");
+            }
+            else if (amount < 0)
+            {
+                Console.WriteLine($"Decrease: {Math.Abs(amount):f2}");
+            }
+        }
+
+        Console.WriteLine($"Balance: {balance:f2}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
