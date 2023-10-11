9a3408767321567109e35f8af5cddcfb4149bb31
Daniel Minchev <lbdidko@gmail.com>
2023-10-12 00:05:11 +0300

    new

---
+++ Nested Loops/03. Triangle of Stars/03. Triangle of Stars/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= number; row += 1)
+{
+    for (int column = 1; column <= row; column += 1)
+    {
+        Console.Write("*");
+    }
+    //string line = new string ('*', row);
+
+    Console.WriteLine();
+}
---
+++ Nested Loops/04. Building/04. Building/Program.cs
@@ -0,0 +1,22 @@
+﻿int floorsCount = int.Parse(Console.ReadLine());
+int roomCount = int.Parse(Console.ReadLine());
+
+for (int floors = floorsCount; floors >= 1; floors --)
+{
+  for(int room = 0; room < roomCount; room ++)
+    {
+        if (floors == floorsCount)
+        {
+            Console.Write($"L{floors}{room} ");
+        }
+        else if (floors % 2 == 0) 
+        {
+            Console.Write($"O{floors}{room} ");
+        }
+        else
+        {
+            Console.Write($"A{floors}{room} ");
+        }
+    }
+    Console.WriteLine();
+}
