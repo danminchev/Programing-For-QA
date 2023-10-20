7a796519fb54ee6f6eeda8aae1493135c8dfc299
Daniel Minchev <lbdidko@gmail.com>
2023-10-21 00:25:56 +0300

    new

---
+++ Arrays/05. Equal Arrays/05. Equal Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] firstArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secontArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+bool isEqual = true;
+
+for (int index = 0; index <= firstArray.Length - 1; index++)
+{
+    if (firstArray[index] != secontArray[index])
+    {
+        isEqual = false;
+        Console.WriteLine("Arrays are not identical.");
+        break;
+
+    }
+}
+if (isEqual)
+Console.WriteLine("Arrays are identical.");
+
---
+++ Arrays/06. Common Elements/06. Common Elements/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] firstArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray(); 
+int[] secondArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int sum = 0;
+
+foreach (int index in firstArray)
+{
+    foreach (int index2 in secondArray)
+    {
+        if (index == index2)
+        {
+            Console.Write(index + " ");
+            break;
+        }
+    }
+}
