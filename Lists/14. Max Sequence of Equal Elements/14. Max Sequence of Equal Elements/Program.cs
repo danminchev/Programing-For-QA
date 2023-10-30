1d9b71c8819f990105f5abfdfe8653089bac67b2
Daniel Minchev <lbdidko@gmail.com>
2023-10-30 11:25:44 +0200

    new

---
+++ Lists/14. Max Sequence of Equal Elements/14. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int bestStart = 0;
+        int bestLength = 1;
+        int currentStart = 0;
+        int currentLength = 1;
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] == numbers[i - 1])
+            {
+                currentLength++;
+
+                if (currentLength > bestLength)
+                {
+                    bestStart = currentStart;
+                    bestLength = currentLength;
+                }
+            }
+            else
+            {
+                currentStart = i;
+                currentLength = 1;
+            }
+        }
+
+        int[] longestSequence = numbers
+            .Skip(bestStart)
+            .Take(bestLength)
+            .ToArray();
+
+        Console.WriteLine(string.Join(" ", longestSequence));
+    }
+}
---
+++ Lists/15. Top Integers/15. Top Integers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        List<int> topIntegers = new List<int>();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            bool isTopInteger = true;
+            int currentNumber = numbers[i];
+
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                if (currentNumber <= numbers[j])
+                {
+                    isTopInteger = false;
+                    break;
+                }
+            }
+
+            if (isTopInteger)
+            {
+                topIntegers.Add(currentNumber);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", topIntegers));
+    }
+}
+
