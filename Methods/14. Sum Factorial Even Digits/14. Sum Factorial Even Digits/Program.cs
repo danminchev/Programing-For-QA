1c96dcde1c3ceee76fce6a32c56ed985fbb9bc26
Daniel Minchev <lbdidko@gmail.com>
2023-10-31 22:15:58 +0200

    new

---
+++ Methods/14. Sum Factorial Even Digits/14. Sum Factorial Even Digits/Program.cs
@@ -0,0 +1,33 @@
+﻿static int CalculateFactorials(int number)
+{
+    int result = 1;
+
+    while (number > 0)
+    {
+        result = result * number;
+        number -= 1;
+    }
+
+    return result;
+}
+
+int number = int.Parse(Console.ReadLine());
+
+int totalResult = 0;
+
+while (number != 0)
+{
+    int digit = number % 10;
+
+    if (digit % 2 == 0)
+    {
+        int resultFactorial = CalculateFactorials(digit);
+        totalResult += resultFactorial;
+    }
+    number = number / 10;
+}
+Console.WriteLine(totalResult);
+
+
+
+
---
+++ Methods/15. Middle Elements/15. Middle Elements/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] inputArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+int middleRightElementIndex = inputArray.Length / 2;
+int middleLeftElementIndex = middleRightElementIndex - 1;
+
+double middleElementAverageg = (inputArray[middleRightElementIndex] + inputArray[middleLeftElementIndex]) / 2.0;
+
+Console.WriteLine($"{middleElementAverageg:F2}");
+
