ab07c995de7d4654e94f91d767005993cf7bcbea
Daniel Minchev <lbdidko@gmail.com>
2023-11-01 23:41:12 +0200

    new

---
+++ Methods/17. Magic Numbers/17. Magic Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+bool foundMagicNumber = false;
+
+for (int number = 1; number <= n; number++)
+{
+    int currentNumber = number;
+    int sumOfDigits = 0;
+    bool isMagic = true;
+
+    while (currentNumber > 0)
+    {
+        int digit = currentNumber % 10;
+
+        if (digit != 2 && digit != 3 && digit != 5 && digit != 7)
+        {
+            isMagic = false;
+            break;  
+        }
+
+        sumOfDigits += digit;
+        currentNumber /= 10;
+    }
+
+    if (isMagic && sumOfDigits % 2 == 0)
+    {
+        Console.Write(number + " ");
+        foundMagicNumber = true;
+    }
+}
+
+if (!foundMagicNumber)
+{
+    Console.WriteLine("no");
+}
+else
+{
+    Console.WriteLine();
+}
---
+++ Methods/18. Min - Max Values/18. Min - Max Values/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int num = int.Parse(Console.ReadLine());
+
+if (num <= numbers.Length)
+{
+    int maxNumber = int.MinValue;
+    int minNumber = int.MaxValue;
+
+    for (int i = 0; i < num; i++)
+    {
+        if (numbers[i] > maxNumber)
+        {
+            maxNumber = numbers[i];
+        }
+        if (numbers[i] < minNumber)
+        {
+            minNumber = numbers[i];
+        }
+    }
+    Console.WriteLine(maxNumber);
+    Console.WriteLine(minNumber);
+}
+
+
+
