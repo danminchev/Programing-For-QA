9360681b51933324f4bc97838e2db79c45b4ce86
Daniel Minchev <lbdidko@gmail.com>
2023-11-05 09:29:38 +0200

    new

---
+++ Arrays/07.  Rotations/07.  Rotations/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int N = int.Parse(Console.ReadLine());
+
+int length = numbers.Length;
+
+for (int rotation = 0; rotation < N; rotation++)
+{
+    int lastNumber = numbers[length - 1];
+
+    for (int i = length - 1; i > 0; i--)
+    {
+        numbers[i] = numbers[i - 1];
+    }
+
+    numbers[0] = lastNumber;
+}
+
+Console.WriteLine(string.Join(", ", numbers));
