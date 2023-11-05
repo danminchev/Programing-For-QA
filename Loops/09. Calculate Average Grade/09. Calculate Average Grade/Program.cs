8fb897d941d7fe94a9f4c3b9390b68d59a70edab
Daniel Minchev <lbdidko@gmail.com>
2023-11-05 09:18:45 +0200

    new

---
+++ Loops/09. Calculate Average Grade/09. Calculate Average Grade/Program.cs
@@ -0,0 +1,14 @@
+﻿int countStudents = int.Parse(Console.ReadLine());
+
+double totalGrade = 0;
+
+for (int i = 0; i < countStudents; i++)
+{
+    double grate = double.Parse(Console.ReadLine());
+
+    totalGrade += grate;
+}
+double averageGrade = totalGrade / countStudents;
+
+Console.WriteLine($"{averageGrade:F2}");
+
